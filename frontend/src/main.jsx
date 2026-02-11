1c3e3e181ce7b90dbdde3a3167029c22c8731c2a
p3rtub4d00 <containerbeerpvh@gmail.com>
2026-02-11 13:00:26 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'  <-- REMOVIDO PARA CORRIGIR O ERRO DE BUILD
+// import './index.css'  <-- ESSA LINHA FOI REMOVIDA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
